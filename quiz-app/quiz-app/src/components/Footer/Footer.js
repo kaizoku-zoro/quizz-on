6f0aa42d5f9dc3b2e797609a863e6e4a7a849536
Nikunj Sharma <nikunj.sharma251@gmail.com>
2021-07-04 22:29:25 +0530

    Add files via upload

---
+++ quiz-app/quiz-app/src/components/Footer/Footer.js
@@ -0,0 +1,20 @@
+const Footer = () => {
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        marginBottom: 10,
+      }}
+    >
+      
+      <a
+        href="https://github.com/kaizoku-zoro/"
+        style={{ cursor: "pointer" }}
+      >
+       <img src="github.png" height="20px"></img>
+      </a>
+    </div>
+  );
+};
+
+export default Footer;
